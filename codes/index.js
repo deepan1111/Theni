d2ccc5b1d2f00bbf2f63394c24c1ab8a8fbe6a2e
deepan <deepanb777@gmail.com>
2023-08-04 09:58:19 +0530

    updated newfile

--- codes/index.js
+++
@@ -1,9 +0,0 @@
-const menu = document.getElementsByClassName("ham")[0]
-const menuList = document.getElementsByClassName("nav-links")[0]
-
-menu.addEventListener("click", ()=>{
-    menu.addEventListener('click',()=>{
-        menuList.classList.toggle("active");
-    })
-})
-
